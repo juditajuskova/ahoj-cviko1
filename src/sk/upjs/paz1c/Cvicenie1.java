3a1cd291e1ec362b0fe04790b26ca6318b1ba25e
student <student@158.197.35.80>
2016-09-23 07:52:46 +0200

    vypis ide 5x

--- src/sk/upjs/paz1c/Cvicenie1.java
+++ src/sk/upjs/paz1c/Cvicenie1.java
@@ -15,7 +15,9 @@ public class Cvicenie1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        for (int i= 0; i < 5; i++){
         System.out.println("Ahoj prve cvicenie!");
+    }
     }
     
 }
