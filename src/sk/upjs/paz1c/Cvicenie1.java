38224d09ec133fea8a3cef547217645372a6eef8
student <student@158.197.35.80>
2016-09-23 07:42:59 +0200

    prvy vypis

---
+++ src/sk/upjs/paz1c/Cvicenie1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sk.upjs.paz1c;
+
+/**
+ *
+ * @author student
+ */
+public class Cvicenie1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ahoj prve cvicenie!");
+    }
+    
+}
